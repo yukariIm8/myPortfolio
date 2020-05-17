532eef84ac69415cb123ae4448612311d6785a3f
yukariIm8 <55606330+miirakuy@users.noreply.github.com>
2020-05-17 15:40:28 -0700

    style: modify icon's desing for hovering.

--- src/Header.js
+++ src/Header.js
@@ -16,10 +16,10 @@ const Header = () => {
   return (
     <div className="wrapper">
       <div className="topbar">
-        <a href="https://github.com/yukariIm8" target="_blank" rel="noopener noreferrer">
-          <img src={github_icon} className="github_icon" alt="go to GitHub" /></a>
-        <a href="https://www.linkedin.com/in/yukari-im/" target="_blank" rel="noopener noreferrer">
-          <img src={linkedin_icon} className="linkedin_icon" alt="go to LinkedIn" /></a>
+          <a href="https://github.com/yukariIm8" target="_blank" rel="noopener noreferrer">
+            <img src={github_icon} className="github_icon" alt="go to GitHub" /></a>
+          <a href="https://www.linkedin.com/in/yukari-im/" target="_blank" rel="noopener noreferrer">
+            <img src={linkedin_icon} className="linkedin_icon" alt="go to LinkedIn" /></a>
       </div>
       <div className="greeting"><h1>Hi, I'm Yukari Im.</h1></div>
       <div className="introduction">
@@ -27,14 +27,14 @@ const Header = () => {
       <div className="profile"><img src={profile_photo} className="profile_photo" alt="yukariim_photo" /></div>
       <div className="experience"><h3>Experience</h3></div>
       <div className="technology">
-        <h4>React, Redux, React Native, JavaScript ES6, Python, Flask APIs, SQL, SQLAlchemy, RBAC, Docker, Kubernetes, Figma, HTML5, CSS3</h4></div>
+        <h4>React, JavaScript ES6, Python, Flask, SQL, SQLAlchemy, RBAC, Kubernetes, Docker, Figma, HTML5, CSS3</h4></div>
       <div className="tech_icon">
         <img src={python_icon} alt="Python" />
         <img src={sql_icon} alt="SQL" />
         <img src={react_icon} alt="React" />
         <img src={javascript_icon} alt="JavaScript" />
-        <img src={css_icon} alt="CSS" />
         <img src={html_icon} alt="HTML" />
+        <img src={css_icon} alt="CSS" />
       </div>
     </div>
     );
